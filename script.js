079f1fde2ae4ff403292913eaff05972a4d4b8ac
Diego <diegooliveiragoncalves43@gmail.com>
2026-07-23 16:40:32 -0300

    Inicia sessao temporizador em tela

--- script.js
+++ script.js
@@ -4,8 +4,21 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const startPauseBt = document.querySelector('#start-pause');
 const musicaFocoInput = document.querySelector('#alternar-musica');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const imagemBtComecar = document.querySelector('.app__card-primary-butto-icon');
+const tempoNaTela = document.querySelector('#timer');
+
 const musica = new Audio('./sons/luna-rise-part-one.mp3');
+const audioPlay = new Audio('./sons/play.wav');
+const audioPause = new Audio('./sons/pause.mp3');
+const audioTimeFinish = new Audio('./sons/beep.mp3');
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
+imagemBtComecar.setAttribute('src', './imagens/play_arrow.png');
 musica.loop = true;
 // adicionando evento de click no input para tocar ou pausar a música
 musicaFocoInput.addEventListener('change', () => {
@@ -61,3 +74,41 @@ function alteraContexto(contexto) {
       break;
   }
 }
+
+const contagemRegressiva = () => {
+  if (tempoDecorridoEmSegundos <= 0) {
+    audioTimeFinish.play();
+    alert('Tempo Finalizado!');
+    zerar();
+    return;
+  }
+  tempoDecorridoEmSegundos--;
+  atualizarTempoNaTela();
+};
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+  if (intervaloId) {
+    audioPause.play();
+    zerar();
+    return;
+  }
+  audioPlay.play();
+  intervaloId = setInterval(contagemRegressiva, 1000);
+  iniciarOuPausarBt.textContent = 'Pausar';
+  imagemBtComecar.setAttribute('src', './imagens/pause.png');
+}
+function zerar() {
+  clearInterval(intervaloId);
+  iniciarOuPausarBt.textContent = 'Retomar';
+  imagemBtComecar.setAttribute('src', './imagens/play_arrow.png');
+  intervaloId = null;
+}
+
+function atualizarTempoNaTela() {
+  const tempo = tempoDecorridoEmSegundos;
+  tempoNaTela.innerHTML = `${tempo}`;
+}
+
+atualizarTempoNaTela();
